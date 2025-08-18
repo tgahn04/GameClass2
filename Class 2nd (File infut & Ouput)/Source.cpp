6d68cc4d0c919314a73abb42a6c5dfdeca50416a
tgahn04 <tgahn0524@gmail.com>
2025-08-18 19:49:47 +0900

    (2025.8.18) 키보드 시스템

---
+++ Class 2nd (File infut & Ouput)/Source.cpp
@@ -0,0 +1,80 @@
+﻿#include <stdio.h>
+
+#define SIZE 100001
+
+void load(const char* filename)
+{
+    FILE* file = fopen(filename, "r");
+
+    char buffer[SIZE] = { 0, };
+
+    // 1. buffer : 읽을 데이터를 저장할 메모리 
+    // 2. element size : 각 데이터 항목의 크기
+    // 3. element count : 데이터를 읽어올 데이터 항목의 수
+    // 4. stream : 데이터를 읽어올 파일 포인터 
+
+    fread(buffer, 1, SIZE, file);
+
+    printf("%s", buffer);
+
+
+
+    fclose(file);
+
+
+
+}
+
+int main()
+{
+#pragma region 파일 입출력
+
+#pragma region File Write
+
+    // fopen(첫 번째 매개 변수) : 파일의 이름
+    // fopen(두 번째 매개 변수) : 입력 & 출력 모드
+
+    // "r" : 읽기 전용 (파일이 없을 때 : NULL)
+    // "w" : 쓰기 전용 (새로 파일을 생성) 
+    // "a" : 추가 모드 (새로 파일을 생성 - 기존에 있는 파일에 내용을 추가) 
+
+    // FILE * file = fopen("data.txt", "w");
+    // 
+    // fputs("Strength : \n", file);
+    // fputs("Dexterity : \n", file);
+    // fputs("Wisdom : \n", file);
+    // fputs("Intelligence : \n", file);
+    // fputs("Constitution :   ", file);
+    // 
+    // fclose(file);
+
+#pragma endregion
+
+#pragma region File Read
+
+    FILE* file = fopen("Pocketmon.txt", "r");
+
+    char buffer[SIZE] = { 0, };
+
+    // 1. buffer : 읽을 데이터를 저장할 메모리 
+    // 2. element size : 각 데이터 항목의 크기
+    // 3. element count : 데이터를 읽어올 데이터 항목의 수
+    // 4. stream : 데이터를 읽어올 파일 포인터 
+
+    fread(buffer, 1, SIZE, file);
+
+    printf("%s", buffer);
+
+
+
+    fclose(file);
+
+
+    load("Pocketmon.txt", "r");
+
+#pragma endregion
+
+#pragma endregion
+
+    return 0;
+}
--- Program/Program.c
+++ Program/Program.c
@@ -1,9 +1,43 @@
 ﻿#include <stdio.h>
+#define SIZE 10
 
 int main()
 {
-	// git branch...
+#pragma region GetAsynckeyState
+	// Window API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
+	// 비동적으로 확인할 때 사용되는 함수입니다.
+
+	// key state
+	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태
+	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태
+	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태
+	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태
+	
+	
+	const char* detective_name = "탐정";
+	const char* client_name = "의뢰인";
+
+	const char* talk[10] = {
+		"탐정 : 누구십니까?",
+		"의뢰인 : 탐정님께 의뢰를 하러왔습니다.",
+		"탐정 : 이쪽으로 오시죠",
+		"의뢰인 : 집에 도둑이 들었는데 범인을 찾아주세요",
+		"탐정 : 알겠습니다. 상황을 알려주세요",
+		"탐정 :
+		"의뢰인 :
+		"탐정 :
+	};
+
+	detective_name ": 누구십니까?",
+		client_name ": 탐정님께 의뢰를 하러왔습니다.",
+		detective_name ": 이쪽으로 오시죠",
+		client_name ": 집에 도둑이 들었는데 범인을 찾아주세요",
+		detective_name ": 알겠습니다. 상황을 알려주세요",
+		client_name ": 어젯밤 집을 비웠는데 그 사이 도둑이 서랍에 있는 금품을 털어갔습니다.",
+		detective_name ": 집주소와 ",
 
 
-return 0;
+#pragma endregion
+
+	return 0;
 }
