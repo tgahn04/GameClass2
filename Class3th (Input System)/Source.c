bb251b55f950ab81cf08c22e9dfcb7afb0c3dea6
tgahn04 <tgahn0524@gmail.com>
2025-08-19 19:45:37 +0900

    (2025.8.19) 좌표 설정 시스템

---
+++ Class3th (Input System)/Source.c
@@ -0,0 +1,53 @@
+﻿#include <stdio.h>
+#include <Windows.h>
+#define SIZE 8
+
+int main()
+{
+#pragma region GetAsyncKeyState
+	// Window API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
+	// 비동적으로 확인할 때 사용되는 함수입니다.
+
+	// key state
+	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태
+	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태
+	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태
+	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태
+
+
+	const char* detective_name = "탐정";
+	const char* client_name = "의뢰인";
+
+	const char* talk[SIZE] = {
+		"탐정 : 누구십니까?",
+		"의뢰인 : 탐정님께 의뢰를 하러왔습니다.",
+		"탐정 : 이쪽으로 오시죠",
+		"의뢰인 : 집에 도둑이 들었는데 범인을 찾아주세요",
+		"탐정 : 알겠습니다. 상황을 알려주세요",
+		"의뢰인 : 어젯밤 집을 비웠는데 그 사이 도둑이 서랍에 있는 금품을 털어갔습니다.",
+		"탐정 : 흥미롭군요. 직접 조사해보겠습니다.",
+		"의뢰인 : 부탁드립니다 탐정님."
+	};
+
+	printf("대화시작\n\n");
+
+	int count = 0;
+
+	while (count < SIZE)
+	{
+		if (GetAsyncKeyState(VK_SPACE) & 0x8000)
+		{
+			printf("%s\n", talk[count]);
+			count++;
+
+			while (GetAsyncKeyState(VK_SPACE) & 0x8000)
+			{
+			}
+		}
+	}
+	printf("\n대화종료\n");
+
+#pragma endregion
+
+	return 0;
+}
--- Program/Program.c
+++ Program/Program.c
@@ -1,43 +1,46 @@
 ﻿#include <stdio.h>
-#define SIZE 10
+#include <Windows.h>
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+void move(int x, int y)
+{
+	// x와 y축을 설정하는 구조체입니다.
+	COORD position = { x, y };
+	
+	// 콘솔 커서의 좌표를 설정하는 함수입니다.
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+}
 
 int main()
 {
-#pragma region GetAsynckeyState
-	// Window API에서 제공하는 입력 처리 함수로, 지정된 키의 상태를
-	// 비동적으로 확인할 때 사용되는 함수입니다.
+	int key;
 
-	// key state
-	// 0x0000 : 이전에 누른 적이 없고, 호출 시점에도 눌려있지 않은 상태
-	// 0x0001 : 이전에 누른 적이 있고, 호출 시점에는 눌려있지 않은 상태
-	// 0x8000 : 이전에 누른 적이 없고, 호출 시점에는 눌려있는 상태
-	// 0x8001 : 이전에 누른 적이 있고, 호출 시점에도 눌려있는 상태
-	
-	
-	const char* detective_name = "탐정";
-	const char* client_name = "의뢰인";
+	while (1)
+	{
+		key = _getch();
 
-	const char* talk[10] = {
-		"탐정 : 누구십니까?",
-		"의뢰인 : 탐정님께 의뢰를 하러왔습니다.",
-		"탐정 : 이쪽으로 오시죠",
-		"의뢰인 : 집에 도둑이 들었는데 범인을 찾아주세요",
-		"탐정 : 알겠습니다. 상황을 알려주세요",
-		"탐정 :
-		"의뢰인 :
-		"탐정 :
-	};
+		switch (key)
+		{
+			case UP:
+			printf("↑ ");
+			break;
+			
+			case LEFT:
+			printf("← ");
+			break; 
+			
+			case RIGHT:
+			printf("→ ");
+			break; 
+			
+			case DOWN:
+			printf("↓ ");
+			break; 
+		}
+	}
 
-	detective_name ": 누구십니까?",
-		client_name ": 탐정님께 의뢰를 하러왔습니다.",
-		detective_name ": 이쪽으로 오시죠",
-		client_name ": 집에 도둑이 들었는데 범인을 찾아주세요",
-		detective_name ": 알겠습니다. 상황을 알려주세요",
-		client_name ": 어젯밤 집을 비웠는데 그 사이 도둑이 서랍에 있는 금품을 털어갔습니다.",
-		detective_name ": 집주소와 ",
-
-
-#pragma endregion
-
-	return 0;
+return 0;
 }
